04ed7a38e8a7142ebde255173dad6dd33559ac37
KrishnaBhakthan <krishnabhakthan743@gmail.com>
2024-06-26 23:13:53 +0530

    first commit

---
+++ ja.js
@@ -0,0 +1,147 @@
+//var num=5;//single line commend
+/*multyline
+ commend*/
+ //variables
+ //var ,let,const
+ //var ourname = "krishna";
+ //const pi=3.14
+ //storing values
+ //var a;//variable declaration
+// var b=27;//assignment of variable
+ //console.log(a)
+ //variable name are case sensitive
+ //push for dlt last elmnt in array
+ //shift for firt element
+ //unshft for adding
+code
+
+ //DATA types in javascript
+ //numbers
+ let a=10;
+ let b=15;
+ c=a+b;
+ console.log(c);
+ //strings
+ let k="krishna";
+ let age="20";
+ console.log(k);
+ //boolean
+ let z=true;
+ let y=false;
+ console.log(y);
+ //array
+ var arr=new Array(1,2,3,4);
+ console.log(arr);
+ //add new element
+ arr.push(5);
+ console.log(arr);
+ //pop element
+ arr.pop();
+ console.log(arr);
+ //unshift-for adding in the first position
+ arr.unshift(6);
+ console.log(arr);
+
+ //object method1
+ var ob=new Object();
+   ob.name="krishna";
+   ob.age=20;
+//object method2
+var ob={
+    fname:"krishna",
+    lname:"bhakthan"
+}
+
+
+//operators in javascript
+//arithmetic operators
+let i=40;
+let j=2;
+u=i+j;
+v=i-j;
+w=i*j;
+x=i**j;
+y=i/j;
+f=i%j;
+console.log(u,v,w,x,y,f);
+
+//assignment operators
+let l =10;
+console.log(l);
+l += 5;
+console.log(l);
+l  -= 5;
+console.log(l);
+l  *= 5;
+console.log(l);
+l  /= 5;
+console.log(l);
+l  %= 5;
+console.log(l);
+l  **= 5;
+console.log(l);
+
+
+//comparison operators
+l = 5;
+n = 5;
+if(l == 5){
+console.log(l);
+}
+if(l === n){
+    console.log(l);
+}
+if(l != 6){
+    console.log("not 5");
+}
+if(l !== 6){
+    console.log(l);
+}
+if(l > 4){
+    console.log("5 is greater");
+    }
+if(l < 6){
+        console.log("5 is smaller");
+}
+if(l >= 4){
+    console.log("5 is greater");
+}
+if(l <= 6){
+    console.log("5 is smaller");
+}
+//ternary operator
+let size=20;
+let type=(size>18)?"big":"small";
+console.log(type);
+
+
+//logical operators
+//&&,|| and !
+l=5;
+//logical AND
+if(l < 6 && l!=6 ){
+    console.log("logical and");
+}
+//logical OR
+if(l < 6 || l==6 ){
+    console.log("logical or");
+}
+//logical not
+if(l < 6  && !(l==6) ){
+    console.log("logical not");
+}
+l=7;
+//typeof 
+console.log(typeof l);
+
+
+
+
+    
+    
+    
+    
+
+
+
+
